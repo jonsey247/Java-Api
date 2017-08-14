5cc96a64ef7379f6df41a59d29bb6e0dd42e5e12
James Heggs <james.heggs@closconsultancy.com>
2017-08-14 13:41:58 +0100

    Include ability to save cars and share lecture docs

--- src/main/java/com/northcoders/controller/CarRestController.java
+++ src/main/java/com/northcoders/controller/CarRestController.java
@@ -3,10 +3,7 @@ package com.northcoders.controller;
 import com.northcoders.model.Car;
 import com.northcoders.repository.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,9 +19,14 @@ public class CarRestController {
     }
 
     @RequestMapping(value="/cars/{carId}", method = RequestMethod.GET)
-    public Car getIndividulaCar(@PathVariable Long carId) {
+    public Car getIndividualCar(@PathVariable Long carId) {
         Car car = carRepository.findOne(carId);
 
         return car;
+    }
+
+    @RequestMapping(value="/cars/{carId}", method = RequestMethod.POST)
+    public void saveCar(@RequestBody Car car) {
+        carRepository.save(car);
     }
 }
